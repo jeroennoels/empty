d492a643df4595c13ac535de051543924840f001
Jeroen Noels <jeroen.noels@telenet.be>
2022-11-10 20:28:23 +0100

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find ../granularity/ -name "*.o"  -print0 | xargs -r -0 /bin/rm
-find ../granularity/ -name "*.hi" -print0 | xargs -r -0 /bin/rm
-find ../granularity/ -name "*~"   -print0 | xargs -r -0 /bin/rm
-/bin/rm -f ../granularity/test/Main
+find ../empty/ -name "*.o"  -print0 | xargs -r -0 /bin/rm
+find ../empty/ -name "*.hi" -print0 | xargs -r -0 /bin/rm
+find ../empty/ -name "*~"   -print0 | xargs -r -0 /bin/rm
+/bin/rm -f ../empty/test/Main
